98b73b8467eef889f9a6c716ca868f0a9634681a
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-05-27 12:55:08 -0500

    Added Dependencies setup server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // ngrok http -subdomain=shopify-application 3000
+// Dependencies
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = "https://shopify-application.ngrok.io";
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
