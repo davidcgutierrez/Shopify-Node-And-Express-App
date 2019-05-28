260256ce229d887f284f3b0aada580d95e164142
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-05-28 08:40:39 -0500

    Added Callback Function

--- index.js
+++ index.js
@@ -21,3 +21,48 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+// Install Route
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+// The Callback Route
+app.get('/shopify/callback', (req, res) => {
+  const {
+    shop,
+    hmac,
+    code,
+    state
+  } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    res.status(200).send('Callback route');
+
+    // TODO
+    // Validate request is from Shopify
+    // Exchange temporary code for a permanent access token
+    // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
